1856073b30356e05f83e49249c1a693b14493cf8
Frederik De Laender <frederik.delaender@unamur.be>
2022-09-05 11:58:01 +0200

    Added cyano data

--- Alpha_analysis.R
+++ Alpha_analysis.R
@@ -1,15 +1,16 @@
 library(tidyverse)
-data <- read.csv("ciliates/DIVERCE_TdB_Ciliates_Traits.csv") %>%
+data_cilia <- read.csv("ciliates/DIVERCE_TdB_Ciliates_Traits.csv") %>%
   group_by(ID_spec, Temp, Atrazine) %>%
   summarise_all(mean, na.rm=T) %>%
   select(contains(c("sd_", "ID", "Temp", "Atrazine", "r1", "a11"))) %>%
   pivot_longer(contains(c("sd_")), names_to = "trait", values_to= "sd") %>%
   filter(!grepl('Spiro', ID_spec))
 
-ggplot(data) + 
+ggplot(data_cilia) + 
   theme_classic() + 
   aes(x=log10(sd), y=log10(-a11), 
       col=as_factor(Temp), pch=as_factor(Atrazine)) + 
   geom_point() + 
   facet_grid(cols = vars(trait), scales="free") #, rows = vars(ID_spec)
 
+data_cyano <- load("cyanobacteria/mono_data_clean.RData")
