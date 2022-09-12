a890f0341a6343fa5abcd9c06fd370392574f195
Edinbru <108137386+Edinbru@users.noreply.github.com>
2022-09-12 15:27:16 +0200

    Filtered Loxo 1 for aspect ratio same as Loxo 2. Results in less outliers for morph & movement traits thus more reliable data

--- ciliates/parameter-analysis.R
+++ ciliates/parameter-analysis.R
@@ -18,7 +18,23 @@ library('scales')
 #          trait = replace(trait, trait == 'sd_speed', 'Speed'),
 #          trait = replace(trait, trait == 'sd_linearity', 'Linearity'))
 
-data <- read.csv('DIVERCE_TdB_Ciliates_Traits.csv') %>%
+data1 <- read.csv('C:/Users/debruin/OneDrive - UCL/Bio/UCL/GitHub/TeamWork/data-analysis-experiments/ciliates/DIVERCE_TdB_Ciliates_Traits.csv') %>%
+  group_by(ID_spec, Temp, Atrazine) %>%
+  summarise(across(where(is.numeric), mean, na.rm = TRUE)) %>%
+  select(contains(c('sd_', 'mean_', 'ID', 'Temp', 'Atrazine', 'r1', 'a11'))) %>%
+  pivot_longer(contains(c('sd_', 'mean_')), 
+               names_to = c('metric', 'trait'), 
+               names_pattern = '(.*)_(.*)') %>%
+  pivot_wider(names_from = metric, values_from = value) %>%
+  #dplyr::filter(!grepl('Spiro', ID_spec)) %>%
+  mutate(trait = replace(trait, trait == 'sd_ar', 'Aspect ratio'),
+         trait = replace(trait, trait == 'sd_area', 'Area'),
+         trait = replace(trait, trait == 'sd_speed', 'Speed'),
+         trait = replace(trait, trait == 'sd_linearity', 'Linearity'))
+
+data1 <- data1[-c(1:12),]
+
+data2 <- read.csv('C:/Users/debruin/OneDrive - UCL/Bio/UCL/GitHub/TeamWork/data-analysis-experiments/ciliates/DIVERCE_TdB_Ciliates_Traits_AR_filt_Loxo.csv') %>%
   group_by(ID_spec, Temp, Atrazine) %>%
   summarise(across(where(is.numeric), mean, na.rm = TRUE)) %>%
   select(contains(c('sd_', 'mean_', 'ID', 'Temp', 'Atrazine', 'r1', 'a11'))) %>%
@@ -36,7 +52,22 @@ data <- read.csv('DIVERCE_TdB_Ciliates_Traits.csv') %>%
 dataOUT <- data %>%
   filter(a11 < -1e-8)
 # plot aii with just the traits as facets
-ggplot(dataOUT %>% 
+ggplot(data1 %>% 
+         #filter(ID_spec == "Loxo_1") %>%
+         mutate(Temp = as.factor(ID_spec), Atrazine = as.factor(Atrazine))) + 
+  theme_bw() + 
+  aes(x = sd, y = -a11) + 
+  scale_x_log10() + 
+  scale_y_log10(breaks = trans_breaks('log10', function(x){10^x}),
+                labels = trans_format('log10', scales::math_format(10^.x))) + 
+  geom_smooth(method = 'lm') +
+  geom_point(aes(col = Temp, pch = Atrazine)) + 
+  labs(x = 'Trait standard deviation (SD)', 
+       y = expression(paste('Self-limitation (', alpha['ii'], ')'))) +
+  facet_wrap(.~trait, scales = 'free')
+
+ggplot(data2 %>% 
+         #filter(ID_spec == "Loxo_1") %>%
          mutate(Temp = as.factor(ID_spec), Atrazine = as.factor(Atrazine))) + 
   theme_bw() + 
   aes(x = sd, y = -a11) + 
@@ -51,6 +82,7 @@ ggplot(dataOUT %>%
 
 # plot aii including species as facet
 ggplot(dataOUT %>% 
+         filter(ID_spec == "Loxo_1" | ID_spec == "Loxo_2") %>%
          mutate(Temp = as.factor(Temp), Atrazine = as.factor(Atrazine))) + 
   theme_bw() + 
   aes(x = sd, y = -a11, col = Temp, pch = Atrazine) + 
@@ -66,6 +98,7 @@ ggplot(dataOUT %>%
 
 # plot ri with just the traits as facets
 ggplot(dataOUT %>% 
+         filter(ID_spec == "Loxo_1" | ID_spec == "Loxo_2") %>%
          mutate(Temp = as.factor(Temp), Atrazine = as.factor(Atrazine))) + 
   theme_bw() + 
   aes(x = mean, y = r1) + 
