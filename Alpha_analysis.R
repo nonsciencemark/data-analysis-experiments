0b4c7205f264b0d8aed2ca643369dffecc7560d1
Frederik De Laender <frederik.delaender@unamur.be>
2022-09-28 21:13:38 +0200

    Further tinkering with graphs to show influence of pop size and traits

--- Alpha_analysis.R
+++ Alpha_analysis.R
@@ -6,22 +6,36 @@ library(mgcv)
 
 # CILIATES ------
 ## Import data and compute pcgr----
-data_cilia <- read.csv("ciliates/DIVERCE_TdB_Ciliates_Traits.csv") %>%
+data_cilia <- read.csv("ciliates/OverviewTraitsFull.csv") %>%
   group_by(Atrazine, Temp, ID_spec) %>%
   mutate(pcgr = log(lead(Count, 1)/Count)/(lead(Days_fromstart, 1)-Days_fromstart),
          .after = Count) %>% 
+  mutate(phase = cut_interval(Count, n=3, labels = FALSE)) %>%
   filter(pcgr > -5) %>% #Kick out extremely negative pcgr
   pivot_longer(mean_ar:sd_linearity, names_to="trait") %>%
   separate(trait, into=c("stat", "trait"), sep="_") %>%
-  pivot_wider(names_from=stat, values_from=value)
+  pivot_wider(names_from=stat, values_from=value) %>%
+  mutate(cv=sd/mean)
 
 ggplot(data_cilia) +
+  scale_shape_manual(values = 0:8) +
   theme_bw() + 
-  geom_point(aes(x=mean, y=pcgr, pch=as_factor(ID_spec),
-                 col=as_factor(Treatment))) + 
-  geom_smooth(method=lm, aes(x=mean, y=pcgr, colour=as_factor(Treatment)), 
+  geom_point(aes(x=log10(mean), y=pcgr, pch=as_factor(Treatment),
+                 col=as_factor(phase)), alpha=0.5) + 
+  geom_smooth(method=lm, aes(x=log10(mean), y=pcgr, 
+                             colour=as_factor(phase), lty=as_factor(Treatment)), 
               formula=y ~ poly(x,2), se=F) +
   facet_wrap(vars(trait), scales="free") #, rows = vars(ID_spec)strain ~ 
+
+ggplot(data_cilia) +
+  scale_shape_manual(values = 0:8) +
+  theme_bw() + 
+  geom_point(aes(x=cv, y=pcgr, pch=as_factor(Treatment),
+                 col=as_factor(phase)), alpha=0.5) + 
+  geom_smooth(method=lm, aes(x=cv, y=pcgr, colour=as_factor(phase), 
+                             lty=as_factor(Treatment)), 
+              formula=y ~ poly(x,1), se=F) +
+  facet_wrap(vars(trait), scales="free")
 
 # CYANO ------
 ## Import data and compute pcgr----
@@ -31,22 +45,31 @@ data_cyano <- read.csv("cyanobacteria/mono_data.csv") %>%
   select(-c(date, time)) %>%
   group_by(strain, treat) %>%
   mutate(day = day - min(day)) %>%
+  mutate(phase = cut_interval(population.mean, n=3, labels = FALSE)) %>%
   select(-population.sd) %>%
   mutate(pcgr = log(lead(population.mean,1)/population.mean)/(lead(day,1)-day),
          .after = population.mean) %>%
   pivot_longer(FSC.HLin.mean:NIR.R.HLin.sd, names_to="trait") %>%
   separate(trait, into=c("trait", "stat"), sep="in.") %>%
-  pivot_wider(names_from=stat, values_from=value)
+  pivot_wider(names_from=stat, values_from=value) %>%
+  mutate(cv=sd/mean)
 
 ggplot(data_cyano) +
   theme_bw() + 
-  geom_point(aes(x=log10(population.mean), y=pcgr, pch=as_factor(strain),
-                 col=as_factor(treat))) + 
-  geom_smooth(method=lm, aes(x=log10(population.mean), y=pcgr, 
-                             colour=as_factor(treat)), 
-              formula=y ~ poly(x,1), se=F) +
+  geom_point(aes(x=log10(mean), y=pcgr, pch=as_factor(treat),
+                 col=as_factor(phase)), alpha=0.5) + 
+  geom_smooth(method=lm, aes(x=log10(mean), y=pcgr, 
+                             colour=as_factor(phase), 
+                             lty=as_factor(treat)), 
+              formula=y ~ poly(x,2), se=F) +
   facet_wrap(vars(trait), scales="free") #, rows = vars(ID_spec)strain ~ 
 
-test <- lm(data_cyano, 
-           formula = pcgr ~ log10(population.mean)*as_factor(treat) + as_factor(treat))#test linear model
-plot(predict(test), data_cyano$pcgr)
+ggplot(data_cyano) +
+  theme_bw() + 
+  geom_point(aes(x=cv, y=pcgr, pch=as_factor(treat),
+                 col=as_factor(phase)), alpha=0.5) + 
+  geom_smooth(method=lm, aes(x=cv, y=pcgr, 
+                             colour=as_factor(phase),
+                             lty=as_factor(treat)), 
+              formula=y ~ poly(x,1), se=F) +
+  facet_wrap(vars(trait), scales="free") 
