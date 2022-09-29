6b5cd5b36c8187d05da6a102e7d0f8b3f2f7d5fd
Frederik De Laender <frederik.delaender@unamur.be>
2022-09-29 10:58:49 +0200

    More fidling

--- Alpha_analysis.R
+++ Alpha_analysis.R
@@ -10,7 +10,9 @@ data_cilia <- read.csv("ciliates/OverviewTraitsFull.csv") %>%
   group_by(Atrazine, Temp, ID_spec) %>%
   mutate(pcgr = log(lead(Count, 1)/Count)/(lead(Days_fromstart, 1)-Days_fromstart),
          .after = Count) %>% 
-  mutate(phase = cut_interval(Count, n=3, labels = FALSE)) %>%
+  ungroup() %>%
+  group_by(ID_spec) %>%
+  mutate(phase = cut_interval(log10(Count), n=5, labels = FALSE)) %>%
   filter(pcgr > -5) %>% #Kick out extremely negative pcgr
   pivot_longer(mean_ar:sd_linearity, names_to="trait") %>%
   separate(trait, into=c("stat", "trait"), sep="_") %>%
@@ -20,11 +22,11 @@ data_cilia <- read.csv("ciliates/OverviewTraitsFull.csv") %>%
 ggplot(data_cilia) +
   scale_shape_manual(values = 0:8) +
   theme_bw() + 
-  geom_point(aes(x=log10(mean), y=pcgr, pch=as_factor(Treatment),
+  geom_point(aes(x=mean, y=pcgr, pch=as_factor(Treatment),
                  col=as_factor(phase)), alpha=0.5) + 
-  geom_smooth(method=lm, aes(x=log10(mean), y=pcgr, 
-                             colour=as_factor(phase), lty=as_factor(Treatment)), 
-              formula=y ~ poly(x,2), se=F) +
+  geom_smooth(method=lm, aes(x=mean, y=pcgr, 
+                             colour=as_factor(phase)), 
+              formula=y ~ poly(x,1)) + #, se=F
   facet_wrap(vars(trait), scales="free") #, rows = vars(ID_spec)strain ~ 
 
 ggplot(data_cilia) +
@@ -32,8 +34,7 @@ ggplot(data_cilia) +
   theme_bw() + 
   geom_point(aes(x=cv, y=pcgr, pch=as_factor(Treatment),
                  col=as_factor(phase)), alpha=0.5) + 
-  geom_smooth(method=lm, aes(x=cv, y=pcgr, colour=as_factor(phase), 
-                             lty=as_factor(Treatment)), 
+  geom_smooth(method=lm, aes(x=cv, y=pcgr, colour=as_factor(phase)), 
               formula=y ~ poly(x,1), se=F) +
   facet_wrap(vars(trait), scales="free")
 
@@ -45,10 +46,12 @@ data_cyano <- read.csv("cyanobacteria/mono_data.csv") %>%
   select(-c(date, time)) %>%
   group_by(strain, treat) %>%
   mutate(day = day - min(day)) %>%
-  mutate(phase = cut_interval(population.mean, n=3, labels = FALSE)) %>%
-  select(-population.sd) %>%
   mutate(pcgr = log(lead(population.mean,1)/population.mean)/(lead(day,1)-day),
          .after = population.mean) %>%
+  ungroup() %>%
+  group_by(strain) %>%
+  mutate(phase = cut_interval(log10(population.mean), n=5, labels = FALSE)) %>%
+  select(-population.sd) %>%
   pivot_longer(FSC.HLin.mean:NIR.R.HLin.sd, names_to="trait") %>%
   separate(trait, into=c("trait", "stat"), sep="in.") %>%
   pivot_wider(names_from=stat, values_from=value) %>%
@@ -56,12 +59,11 @@ data_cyano <- read.csv("cyanobacteria/mono_data.csv") %>%
 
 ggplot(data_cyano) +
   theme_bw() + 
-  geom_point(aes(x=log10(mean), y=pcgr, pch=as_factor(treat),
+  geom_point(aes(x=mean, y=pcgr, pch=as_factor(treat),
                  col=as_factor(phase)), alpha=0.5) + 
-  geom_smooth(method=lm, aes(x=log10(mean), y=pcgr, 
-                             colour=as_factor(phase), 
-                             lty=as_factor(treat)), 
-              formula=y ~ poly(x,2), se=F) +
+  geom_smooth(method=lm, aes(x=mean, y=pcgr, 
+                             colour=as_factor(phase)), 
+              formula=y ~ poly(x,1)) + #, se=F
   facet_wrap(vars(trait), scales="free") #, rows = vars(ID_spec)strain ~ 
 
 ggplot(data_cyano) +
@@ -69,7 +71,6 @@ ggplot(data_cyano) +
   geom_point(aes(x=cv, y=pcgr, pch=as_factor(treat),
                  col=as_factor(phase)), alpha=0.5) + 
   geom_smooth(method=lm, aes(x=cv, y=pcgr, 
-                             colour=as_factor(phase),
-                             lty=as_factor(treat)), 
+                             colour=as_factor(phase)), 
               formula=y ~ poly(x,1), se=F) +
   facet_wrap(vars(trait), scales="free") 
