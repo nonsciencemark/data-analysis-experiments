3bc384043befbb8dfde73241e4665e3c49a374ea
Frederik De Laender <frederik.delaender@unamur.be>
2022-10-21 12:06:53 +0200

    Implemented new analysis also for ciliates

--- Alpha_analysis.R
+++ Alpha_analysis.R
@@ -21,63 +21,65 @@ data_cilia <- read.csv("ciliates/OverviewTraitsFull.csv") %>%
   pivot_longer(mean_ar:sd_linearity, names_to="trait") %>%
   separate(trait, into=c("stat", "trait"), sep="_") %>%
   pivot_wider(names_from=stat, values_from=value) %>%
-  mutate(cv=sd/mean)%>%
-  filter(((species=="Spiro")&(trait=="speed"))|((species=="Tetra")&(trait=="speed"))
-         |((species=="Para")&(trait=="speed"))|((species=="Loxo")&(trait=="speed")))
+  mutate(cv=sd/mean)
+#%>%
+#  filter(((species=="Spiro")&(trait=="speed"))|((species=="Tetra")&(trait=="speed"))
+#         |((species=="Para")&(trait=="speed"))|((species=="Loxo")&(trait=="speed")))
 
-## fit a reference model for density dependence of population growth ------
-# This model only uses the control data, cleaned up, i.e. 
-# w/o crashes and lag phases if any. Crash detection happens with a GAM
+##check if dd changes with treatment -------
+ggplot(data_cilia) +
+  scale_colour_manual(values=c("black", cbPalette)) + 
+  aes(x=Parts_permL,y=pcgr, col=as.factor(Treatment)) +
+  geom_point() +
+  facet_grid(cols=vars(strain), scales="free") +
+  geom_smooth(method=lm, aes(x=Parts_permL, y=pcgr, col=as.factor(Treatment)),
+              formula=y ~ poly(x,1), se=F) 
+
+##check if dependence of trait on density changes with treatment -------
+ggplot(data_cilia) +
+  scale_colour_manual(values=c("black", cbPalette)) + 
+  aes(x=log10(Parts_permL),y=log10(mean), col=as.factor(Treatment)) +
+  geom_point() +
+  facet_grid(cols=vars(strain), rows=vars(trait), scales="free") +
+  geom_smooth(method=lm, aes(x=log10(Parts_permL), y=log10(mean), col=as.factor(Treatment)),
+              formula=y ~ poly(x,1), se=F)
+
+## fit a reference model of dd and trait dependence on density--------
+# This model only uses the control data
+# w/o crashes. Crash detection happens with a GAM
 # No apparent lag phases here.
 data_cilia_ref <- data_cilia %>% 
   filter(Treatment==1) 
-
 gam_model <- gam(log10(Parts_permL) ~ strain + 
-             s(Days_fromstart, by = strain, k=5),
-             data = data_cilia_ref)
+                   s(Days_fromstart, by = strain, k=5),
+                 data = data_cilia_ref)
 data_cilia_ref$predictions <- predict.gam(gam_model) 
 data_cilia_ref <- data_cilia_ref %>%
   group_by(strain) %>%
   filter(Days_fromstart < max(Days_fromstart*(predictions==max(predictions))))
+###first dd----------------
+ref_model_dd <- lm(data_cilia_ref, formula = pcgr ~ poly(Parts_permL,1)*strain + strain)
 
-ref_model <- lm(data_cilia_ref, 
-                formula = pcgr ~ poly(Parts_permL,1)*strain + strain)
-
-## fit a reference model for the trait dynamics ------
-ref_model_trait <- gam(log10(mean) ~ strain + 
-                   s(log10(Parts_permL), by = strain, k=5),
-                   data = data_cilia_ref)
-#add predictions to the original data frame
-data_cilia$pcgr_ref <- predict.lm(ref_model, newdata = data_cilia)
-data_cilia$mean_ref <- 10^predict.gam(ref_model_trait, newdata = data_cilia)
-
+###then traits----------------
+ref_model_trait <- lm(data_cilia_ref,  
+                      formula = mean ~ poly(log10(Parts_permL),1)*strain*trait + strain*trait + strain + trait)
+###add predictions to the original data frame------------
+data_cilia$pcgr_ref <- predict.lm(ref_model_dd, newdata = data_cilia)
+data_cilia$mean_ref <- predict.lm(ref_model_trait, newdata = data_cilia)
 data_cilia <- data_cilia %>%
   mutate(delta_pcgr = pcgr-pcgr_ref) %>%
   mutate(delta_trait = mean-mean_ref)
-#plot the data and overlay the reference model of dd
-ggplot(data_cilia) +
-  theme_bw() + 
-  geom_point(aes(x=Days_fromstart, y=mean)) + 
-  geom_smooth(method=lm, aes(x=Days_fromstart, y=mean), 
-              formula=y ~ poly(x,1), se=F) + #
-  geom_line(aes(x=Days_fromstart, y=mean_ref)) +
-  facet_wrap(vars(Treatment, strain), scales="free")
 
-#now check if the deviation from the ref model (delta) 
-#depends on the trait value
+##now check if delta with ref model depends on the trait----------
 ggplot(data_cilia) +
   theme_bw() + 
-  scale_size_manual(values=c(3, rep(1,10))) +
-  scale_colour_manual(values=c(cbPalette, "purple")) + 
-  geom_point(aes(x=delta_trait, y=delta_pcgr, col=as_factor(Treatment),
-                 size=as_factor(Treatment)),
-             alpha=0.5) + 
-  geom_smooth(method=lm, aes(x=delta_trait, y=delta_pcgr, col=as_factor(Treatment)),
-              formula=y ~ poly(x,1), se=F) + 
-  geom_smooth(method=lm, aes(x=delta_trait, y=delta_pcgr),
-              formula=y ~ poly(x,1), colour="black") +
-  facet_wrap(vars(strain), scales="free") #, rows = vars(ID_spec)strain ~ 
- 
+  scale_size_manual(values=c(rep(1,2), 3, rep(1,10))) +
+  scale_colour_manual(values=c("black", cbPalette)) + 
+  geom_point(aes(x=delta_trait, y=delta_pcgr, col=as.factor(Treatment)), 
+             alpha=0.8) + 
+  geom_smooth(method=lm, aes(x=delta_trait, y=delta_pcgr, col=as.factor(Treatment)),#
+              formula=y ~ poly(x,1)) + #,  col="black"
+  facet_grid(rows=vars(species), cols=vars(trait), scales="free")
 
 # CYANO ------
 ## Import data and compute pcgr----
@@ -108,7 +110,7 @@ data_cyano <- read.csv("cyanobacteria/mono_data.csv") %>%
 ggplot(data_cyano) +
   aes(x=population.mean,y=pcgr, col=treat) +
   geom_point() +
-  facet_grid(cols=vars(strain), rows=vars(trait), scales="free") +
+  facet_grid(cols=vars(strain), scales="free") +
   geom_smooth(method=lm, aes(x=population.mean, y=pcgr, col=treat),
               formula=y ~ poly(x,1), se=F) 
 
@@ -139,7 +141,7 @@ data_cyano <- data_cyano %>%
   mutate(delta_pcgr = pcgr-pcgr_ref) %>%
   mutate(delta_trait = log10(mean/mean_ref))
 
-#now check if delta with ref model depends on the trait
+##now check if delta with ref model depends on the trait----------
 ggplot(data_cyano) +
   theme_bw() + 
   scale_size_manual(values=c(rep(1,2), 3, rep(1,10))) +
@@ -148,6 +150,6 @@ ggplot(data_cyano) +
              alpha=0.8) + 
   geom_smooth(method=lm, aes(x=delta_trait, y=delta_pcgr, col=treat),#
               formula=y ~ poly(x,1)) + #,  col="black"
-  facet_grid(cols=vars(species), rows=vars(trait), scales="free")
+  facet_grid(rows=vars(species), cols=vars(trait), scales="free")
   
 
