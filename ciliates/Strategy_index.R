ec970de982cd90090fcef4cacaeb72510477e777
Edinbru <108137386+Edinbru@users.noreply.github.com>
2022-10-27 15:44:49 +0200

    change in standardization
    Changed from by species to by strains

--- ciliates/Strategy_index.R
+++ ciliates/Strategy_index.R
@@ -21,7 +21,7 @@ data_cilia <-  read.csv("C:/Users/debruin/OneDrive - UCL/Bio/UCL/GitHub/TeamWork
   filter(pcgr > -5) %>% #Kick out extremely negative pcgr
   ungroup() %>%
   mutate(Species = as.factor(str_replace_all(str_sub(ID_spec, start = 1, end = 5), "[[:punct:]]", ""))) %>%
-  group_by(Species) %>%
+  group_by(ID_spec) %>%
   mutate_at(c("mean_speed", "mean_area", "mean_ar", "mean_linearity", "sd_speed", "sd_area", "sd_ar", "sd_linearity", "pcgr", "Parts_permL", "Days_fromstart"), ~ (scale(.) %>% as.vector)) %>%
   ungroup() %>%
   mutate(Strategy_Go = (mean_speed + mean_linearity), 
@@ -39,39 +39,56 @@ data_cilia$Species <- as.factor(data_cilia$Species)
 
 # Plots:
 ggplot(data = data_cilia, mapping = aes(x = Atrazine, y = Strategy_Go, color = ID_spec)) +
-  geom_point(aes(shape = Temp), stat = "summary", size = 3, show.legend = TRUE) +
+  stat_summary(fun.data = "mean_cl_normal",
+               geom = "pointrange") +
+  geom_hline(yintercept = 0, lty = 2, color = "grey") +
   facet_wrap(facets = .~ Species) +
   labs() +
   theme_classic(base_size = 24)
 
 ggplot(data = data_cilia, mapping = aes(x = Atrazine, y = Strategy_Stay, color = ID_spec)) +
-  geom_point(aes(shape = Temp), stat = "summary", size = 3, show.legend = TRUE) +
+  stat_summary(fun.data = "mean_cl_normal",
+               geom = "pointrange") +
+  geom_hline(yintercept = 0, lty = 2, color = "grey") +
   facet_wrap(facets = .~ Species) +
   labs() +
   theme_classic(base_size = 24)
 
 
-ggplot(data = data_cilia, mapping = aes(x = Strategy_Stay, y = Strategy_Go)) +
-  geom_point(aes(color = Temp, shape = Atrazine), stat = "summary", size = 2, show.legend = TRUE) +
+ggplot(data = data_cilia, mapping = aes(x = Strategy_Stay, y = Strategy_Go), color = as.factor(Temp)) +
+  geom_point(aes(color = Temp) )+  
   facet_wrap(facets = .~ ID_spec) +
-  geom_smooth(method = "lm") +
+  geom_smooth(aes(group = Temp, color = Temp), method = "lm") +
+  geom_hline(yintercept = 0, lty = 2, color = "grey") +
   labs() +
   theme_classic(base_size = 24)
 
 ggplot(data = data_cilia, mapping = aes(x = Atrazine, y = Strategy_index, color = ID_spec)) +
-  geom_point(aes(shape = Temp, color = ID_spec), stat = "summary", size = 3, show.legend = TRUE) +
-  stat_summary(fun.data = mean_cl_normal,
-               geom = "errorbar", 
-               width = 0.15) +
+  stat_summary(fun.data = "mean_cl_normal",
+               geom = "pointrange") +
   facet_wrap(facets = .~ Species + Temp) +
   geom_hline(yintercept = 0, lty = 2, color = "grey") +
   labs() +
   theme_classic(base_size = 24)
 
-ggplot(data = data_cilia, mapping = aes(x = pcgr, y = Strategy_Go)) +
-  geom_point(aes(shape = Temp, color = ID_spec), stat = "summary", size = 1, show.legend = TRUE) +
-  geom_smooth(aes(group = ID_spec, color = ID_spec), se = F) +
-  facet_wrap(facets = .~ Species + Temp) +
+ggplot(data = subset(data_cilia, Species == "Loxo" | Species == "Para"), mapping = aes(x = pcgr, y = Strategy_Go)) +
+  stat_summary(aes(x = pcgr, y = Strategy_Go, shape = Temp, color = Atrazine), 
+               fun.data = "mean_se",
+               geom = "point", 
+               show.legend = TRUE) +
+  geom_smooth(aes(group = Atrazine, color = Atrazine), method = "loess", se = F) +
+  facet_wrap(facets = .~ ID_spec + Temp) +
+  geom_hline(yintercept = 0, lty = 2, color = "grey") +
+  labs() +
+  theme_classic(base_size = 24)
+
+ggplot(data = subset(data_cilia, Species == "Spiro" | Species == "Tetra"), mapping = aes(x = pcgr, y = Strategy_Go)) +
+  stat_summary(aes(x = pcgr, y = Strategy_Go, shape = Temp, color = Atrazine), 
+               fun.data = "mean_se",
+               geom = "point", 
+               show.legend = TRUE) +
+  geom_smooth(aes(group = Atrazine, color = Atrazine), method = "lm", se = T) +
+  facet_wrap(facets = .~ ID_spec + Temp) +
   geom_hline(yintercept = 0, lty = 2, color = "grey") +
   labs() +
   theme_classic(base_size = 24)
@@ -84,10 +101,24 @@ ggplot(data = data_cilia, mapping = aes(x = pcgr, y = Strategy_Stay)) +
   labs() +
   theme_classic(base_size = 24)
 
-ggplot(data = data_cilia, mapping = aes(x = pcgr, y = Strategy_index)) +
-  geom_point(aes(color = ID_spec), stat = "summary", size = 1, show.legend = TRUE) +
-  geom_smooth(aes(group = ID_spec, color = ID_spec), se = F) +
-  facet_wrap(facets = .~  Treatment) +
+ggplot(data = subset(data_cilia, Species == "Spiro" | Species == "Tetra"), mapping = aes(x = pcgr, y = Strategy_index)) +
+  stat_summary(aes(x = pcgr, y = Strategy_index, color = Atrazine), 
+               fun.data = "mean_se",
+               geom = "point", 
+               show.legend = TRUE) +
+  geom_smooth(aes(group = Atrazine, color = Atrazine), method = "loess", se = F) +
+  facet_wrap(facets = .~ ID_spec + Temp) +
+  geom_hline(yintercept = 0, lty = 2, color = "grey") +
+  labs() +
+  theme_classic(base_size = 24)
+
+ggplot(data = subset(data_cilia, Species == "Loxo" | Species == "Para"), mapping = aes(x = pcgr, y = Strategy_index)) +
+  stat_summary(aes(x = pcgr, y = Strategy_index, color = Atrazine), 
+               fun.data = "mean_se",
+               geom = "point", 
+               show.legend = TRUE) +
+  geom_smooth(aes(group = Atrazine, color = Atrazine), method = "loess", se = F) +
+  facet_wrap(facets = .~ ID_spec + Temp) +
   geom_hline(yintercept = 0, lty = 2, color = "grey") +
   labs() +
   theme_classic(base_size = 24)
