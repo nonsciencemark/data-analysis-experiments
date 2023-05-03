8e06f43fdb18443936b1541511553855550d1c58
Frederik De Laender <frederik.delaender@unamur.be>
2023-05-03 10:23:38 +0200

    Adjusted rest of analysis to cope with pca instead of separate traits

--- Analysis.R
+++ Analysis.R
@@ -4,58 +4,69 @@
 # IMPORT TOOLS AND DATA -----------------------
 source("Tools and data.R")
 # PICK DATA SOURCE ---------------
-model_system <- "cyano" #cilia or cyano
+model_system <- "cilia" #cilia or cyano
 data         <- get(paste("data_",model_system, sep=""))
 # DO ANALYSES --------------------
+## Check correlations between traits and abundance -----
+ggplot(data) + 
+  scale_shape_manual(values=0:10) + 
+  theme_bw() + 
+  scale_colour_manual(values=cbPalette) + 
+  aes(x=log10(density), y=trait, col=as.factor(treat)) + 
+  #aes(x=response, y=delta_AIC, col=as.factor(treat), pch=trait) + 
+  #geom_jitter(width = 0.25) +
+  geom_point(pch=1) +
+  #geom_smooth(method="lm", se=F) +
+  facet_wrap(vars(strain), ncol=2)
+
+# Conclusion: not a very strong link between traits and abundance, with some exceptions for the cyanos
+
 ## How well can we predict pcgr and trait change?-----
 stats_result <- modelling(data=data, 
-                          var_to_nest_by = c("strain", "trait", "treat"),
-                          formulas=c("dT ~ density + mean", #+ density*mean
-                                     "dT ~ mean",
-                                     "pcgr ~ mean + density",# + density*mean
+                          var_to_nest_by = c("strain", "treat"),
+                          formulas=c("dT ~ density + trait", #+ density*mean
+                                     "dT ~ trait",
+                                     "pcgr ~ trait + density",# + density*mean
                                      "pcgr ~ density")) %>%
   select(-data)
 
 #Now join these models to all the data and make the predictions
 data_preds <- data %>%
   ungroup() %>%
-  nest_by(species, strain, trait, treat) %>%
-  left_join(stats_result, by=c("strain", "trait", "treat"), multiple = "all") %>%
+  nest_by(species, strain, treat) %>%
+  left_join(stats_result, by=c("strain", "treat"), multiple = "all") %>%
   mutate(predictions = list(cbind(data, pred=predict.lm(model, newdata=data)))) %>%
   rowwise() %>%
   mutate(AIC=AIC(model)) %>%  
   ungroup() %>%
-  mutate(response = case_when(form=="dT ~ density + mean" ~ "trait change",
-                              form=="dT ~ mean" ~ "trait change",
-                              form=="pcgr ~ mean + density" ~ "growth",
+  mutate(response = case_when(form=="dT ~ density + trait" ~ "trait change",
+                              form=="dT ~ trait" ~ "trait change",
+                              form=="pcgr ~ trait + density" ~ "growth",
                               form=="pcgr ~ density" ~ "growth")) %>%
   ungroup()
   
 # Plot model fit and compare (AIC)
 data_preds_synth <- data_preds %>%
-  mutate(predictors = case_when(form=="dT ~ density + mean" ~ "both",
-                                form=="dT ~ mean" ~ "focal",
-                                form=="pcgr ~ mean + density" ~ "both",
+  mutate(predictors = case_when(form=="dT ~ density + trait" ~ "both",
+                                form=="dT ~ trait" ~ "focal",
+                                form=="pcgr ~ trait + density" ~ "both",
                                 form=="pcgr ~ density" ~ "focal")) %>%
-  select(c("species", "strain", "trait", "treat", "response", "AIC", "predictors")) %>%
+  select(c("species", "strain", "treat", "response", "AIC", "predictors")) %>%
   pivot_wider(names_from = "predictors", values_from = "AIC") %>%
   mutate(delta_AIC = both - focal) #if <0 then both predict better
 
-response <- "trait change"#growth or trait change
-xlab <- ifelse(response=="trait change", 
-               "AIC, trait only", "AIC, abundance only")
-ggplot(data_preds_synth %>% filter(response==response)) + 
+ggplot(data_preds_synth) + 
   scale_shape_manual(values=0:10) + 
   theme_bw() + 
   scale_colour_manual(values=cbPalette) + 
-  aes(x=focal, y=both, col=as.factor(treat), pch=trait) + 
+  aes(x=focal, y=both, col=as.factor(treat), pch=response) + 
   #aes(x=response, y=delta_AIC, col=as.factor(treat), pch=trait) + 
   #geom_jitter(width = 0.25) +
   geom_point() +
   #geom_hline(yintercept = 0, lty="dotted") +
   geom_abline(intercept = 0, slope=1, lty="dotted") +
   facet_wrap(vars(strain), ncol=2, scales="free") + 
-  labs(x = xlab, y = "AIC, both predictors", col="treatment", pch="trait")
+  labs(x = "AIC, trait or abundance only", y = "AIC, both predictors", col="treatment", pch="trait")
   
 #ggsave(paste0(model_system, "_", response, ".pdf"), 
 #width=5, height = 4, device = "pdf")
@@ -86,14 +97,3 @@ ggplot(data_preds %>% filter(response=="trait change")) +
   facet_wrap(trait~form, scales="free") + #
   geom_abline(slope=1, intercept=0) 
 
-## Check correlations between traits and abundance -----
-ggplot(data) + 
-  scale_shape_manual(values=0:10) + 
-  theme_bw() + 
-  scale_colour_manual(values=cbPalette) + 
-  aes(x=log10(density), y=trait, col=as.factor(treat), pch=strain) + 
-  #aes(x=response, y=delta_AIC, col=as.factor(treat), pch=trait) + 
-  #geom_jitter(width = 0.25) +
-  geom_point() +
-  #geom_smooth(method="lm", se=F) +
-  facet_wrap(vars(strain), ncol=2)
