a54efd4fa68da2eb5ffd6517b84bbcc9a7ee5bc8
MARK HOLMES <mark.holmes@unamur.be>
2023-06-22 17:22:20 +0200

    file split and plots improve
    i can't read this many lines, moved plotting functions to new file and call them from main analysis. might want to turn this into a markdown file

--- Analysis.R
+++ Analysis.R
@@ -1,280 +1,72 @@
 #devtools::install_github("ctkremer/growthTools")
 #library(growthTools)
 
-# IMPORT TOOLS AND DATA -----------------------
+# IMPORT TOOLS AND DATA ----
 source("Tools and data.R")
+source("Plotting functions.R")
 
-# PICK DATA SOURCE ---------------
-model_system <- "cilia" #cilia or cyano
-data         <- get(paste("data_",model_system, sep=""))
+# PICK DATA SOURCE ----
+# model_system <- "cilia" #cilia or cyano
 
-# DO ANALYSES --------------------
-## Just plot of dT vs. trait and pcgr vs. pop. ------
-ggplot(data) +
-  theme_bw() + 
-  scale_colour_manual(values=cbPalette) + 
-  aes(x=density, y=pcgr, col=treat) +
-  geom_point() +
-  geom_smooth(method="lm", se=F, lwd=0.5) +
-  facet_wrap(vars(strain), ncol=2, scales="free") 
+# PLOT BASIC OUTPUTS ----
+basic_plots("cyano")
+basic_plots("cilia")
 
-ggsave(paste0("plots/", model_system,"_pcgr.pdf"), 
-       width=5, height = 4, device = "pdf")
+## Results ----
 
-ggplot(data) +
-  theme_bw() + 
-  scale_colour_manual(values=cbPalette) + 
-  aes(x=trait, y=dT, col=treat) +
-  geom_point() +
-  geom_smooth(method="lm", se=F, lwd=0.5) +
-  facet_wrap(vars(strain), ncol=2, scales="free") 
+# Ciliates: 
+# negative dd, but different for different treatments; 
+# negative td, not different for different treatments.
 
-ggsave(paste0("plots/", model_system,"_dT.pdf"), 
-       width=5, height = 4, device = "pdf")
+# Cyanos:
+# dd (mostly negative); different for different treatments. 
+# td (mostly negative); different for different treatments.
 
-###Result--------
-#Ciliates: 
-#negative dd, but different for different treatments; 
-#negative td, not different for different treatments.
-#Cyanos:
-#dd (mostly negative); different for different treatments. 
-#td (mostly negative); different for different treatments.
+# Task 1: Predict next time point within condition: single vs. two variables ----
 
-## Task 1: Predict next time point within condition: single vs. two variables-----
-stats_result <- modelling(data=data, 
-                          var_to_nest_by = c("strain", "treat"),
-                          formulas=c("dT ~ density + trait", #+ density*mean
-                                     "dT ~ trait",
-                                     "pcgr ~ trait + density",# + density*mean
-                                     "pcgr ~ density")) %>%
-  dplyr::select(-data)
+LHSs <- c('dT', 'pcgr')
+RHSs <- c('density', 
+          'trait', 
+          # 'poly(trait, 2)', 
+          'density + trait' # , 
+          # 'density + poly(trait, 2)'
+          )
 
-#Now join these models to all the data and make the predictions
-data_preds <- data %>%
-  ungroup() %>%
-  nest_by(species, strain, treat) %>%
-  left_join(stats_result, by=c("strain", "treat"), multiple = "all") %>%
-  mutate(predictions = list(cbind(data, pred=predict.lm(model, newdata=data)))) %>%
-  rowwise() %>%
-  mutate(AIC=AIC(model)) %>%  
-  ungroup() %>%
-  mutate(response = case_when(form=="dT ~ density + trait" ~ "trait change",
-                              form=="dT ~ trait" ~ "trait change",
-                              form=="pcgr ~ trait + density" ~ "growth",
-                              form=="pcgr ~ density" ~ "growth")) %>%
-  ungroup()
-  
-# Plot model fit and compare (AIC)
-library('qpcR')
+eqs <- expand.grid('LHS' = LHSs, 'RHS' = RHSs) %>%
+  # ensure that we're not having e.g. pcgr ~ trait
+  dplyr::filter(LHS == 'dT' & grepl('trait', RHS) | 
+                  LHS == 'pcgr' & grepl('density', RHS)) %>%
+  apply(1, function(i) {paste0(i[1], ' ~ ', i[2])}) %>%
+  sort()
 
-data_preds_synth <- data_preds %>%
-  mutate(predictors = case_when(form=="dT ~ density + trait" ~ "both",
-                                form=="dT ~ trait" ~ "focal",
-                                form=="pcgr ~ trait + density" ~ "both",
-                                form=="pcgr ~ density" ~ "focal")) %>%
-  dplyr::select(c("species", "strain", "treat", "response", "AIC", "predictors")) %>%
-  pivot_wider(names_from = "predictors", values_from = "AIC") %>%
-  rowwise %>% # group_by(strain, treat, response) %>%
-  mutate(p.better = akaike.weights(c(both, focal))$weights[1]) 
-  # mutate(delta_AIC = max(c(both, focal)) - min(c(both, focal))) #if <0 then both predict better
+AIC_plots("cyano")
+AIC_plots("cilia")
 
- #OOPS THIS HAS STOPPED SHOWING WHICH IS BETTER
+## Results ----
 
-ggplot(data_preds_synth) + 
-  scale_shape_manual(values=0:10) + 
-  theme_bw() + 
-  scale_colour_manual(values=cbPalette) + 
-  aes(x=response, y=p.better, fill=as.factor(treat)) + 
-  #aes(x=response, y=delta_AIC, col=as.factor(treat), pch=trait) + 
-  # ggpattern::geom_rect_pattern(
-  #   xmin = -Inf, xmax = Inf, ymin = 0, ymax = 2, inherit.aes = FALSE,
-  #   data = data.frame(response = 'growth', treatment = 'C'), 
-  #   pattern = 'crosshatch', 
-  #   pattern_fill = 'grey', pattern_colour = 'grey', 
-  #   pattern_density = 0.1, pattern_angle = 45, pattern_alpha = 0.5,
-  #   fill = NA, colour = 'grey') +
-  # geom_jitter(width = 0.25) +
-  geom_col(position = position_dodge(width = 0.5), width = 0.25) +
-  #geom_point() +
-  # geom_hline(yintercept = c(2, 7, 10), lty="dotted") +
-  geom_hline(yintercept = c(0, 1)) +
-  #geom_abline(intercept = 0, slope=1) +
-  facet_wrap(vars(strain), ncol=2, scales="free") + 
-  # scale_y_sqrt() +
-  labs(col = "treatment", 
-       y = expression("Probability that full model outperforms single model"))
-  # labs(y=expression(paste("AIC"[full],"-AIC"[single])), col="treatment")
-  
-#ggsave(paste0(model_system, "_", response, ".pdf"), 
-#width=5, height = 4, device = "pdf")
-if (model_system == 'cyano') {
-  ggsave(paste0("plots/", model_system,"_AIC.pdf"), 
-         width = 4.5, height = 3.5, device = "pdf")
-} else {
-  ggsave(paste0("plots/", model_system,"_AIC.pdf"), # ciliate plot bigger
-         width = 4.5, height = 7, device = "pdf")
-}
+# Ciliates: 
+# Combining two predictors rarely helped to predict growth or trait change
 
-# Plot model fits
-data_preds <- data_preds %>%
-  dplyr::select(-c("data", "model")) %>%
-  unnest(predictions)
-
-#, form=="pcgr ~ density"
-ggplot(data_preds %>% filter(response=="growth")) + 
-  theme_bw() + 
-  scale_shape_manual(values=0:10) + 
-  scale_colour_manual(values=cbPalette) + 
-  aes(x=pcgr, y=pred, col=as.factor(treat), pch=strain) + 
-  geom_point() + 
-  geom_smooth(method="lm", se=F, lwd=0.5)+
-  facet_grid(vars(form)) + #, scales="free"
-  geom_abline(slope=1, intercept=0) 
-
-ggsave(paste0("plots/", model_system,"growth.pdf"), 
-       width = 4, height = 4.5, device = "pdf")
-
-ggplot(data_preds %>% filter(response=="trait change")) + 
-  theme_bw() + 
-  scale_shape_manual(values=0:10) + 
-  scale_colour_manual(values=cbPalette) + 
-  aes(x=dT, y=pred, col=as.factor(treat), pch=strain) + 
-  geom_point() + 
-  geom_smooth(method="lm", se=F, lwd=0.5)+
-  facet_grid(vars(form)) + #
-  geom_abline(slope=1, intercept=0) 
-
-ggsave(paste0("plots/", model_system,"trait.pdf"), 
-       width=5, height = 4, device = "pdf")
-
-###Result--------
-#Ciliates: 
-#Combining two predictors rarely helped to predict growth or trait change
-#Cyanos:
-#Combining two predictors rarely helped to predict growth, but did improve trait change prediction
+# Cyanos:
+# Combining two predictors rarely helped to predict growth, but did improve trait change prediction
 
 ## Task 2: Predict next time point across conditions: single vs. two predictors-----
-stats_result <- modelling(data=data%>%filter(treat=="C"), 
-                          var_to_nest_by = c("strain"),
-                          formulas=c("dT ~ trait", 
-                                     "dT ~ trait + density",
-                                     "pcgr ~ density",
-                                     "pcgr ~ trait + density")) %>%
-  dplyr::select(-data)
 
-#Now join these models to all the data and make the predictions
-data_preds <- data %>%
-  ungroup() %>%
-  nest_by(strain) %>%
-  left_join(stats_result, by=c("strain"), multiple = "all") %>%
-  mutate(response = case_when(length(grep("dT", form))>0 ~ "trait change",
-                              length(grep("pcgr", form))>0 ~ "growth")) %>%
-  mutate(predictor = case_when(length(grep("trait + density", form, fixed=T))>0 ~ "both",
-                               length(grep("density", form))>0 ~ "one",
-                               length(grep("trait", form))>0 ~ "one")) %>%
-  #dplyr::select(-form) %>%
-  mutate(data_and_pred = list(cbind(data, prediction=predict.lm(object=model, newdata=data)))) %>%
-  dplyr::select(c("strain", "response", "form", "predictor", "data_and_pred")) %>%
-  unnest(c("data_and_pred")) %>%
-  #pivot_wider(values_from = "prediction", names_from = "response")
-  mutate(error =  case_when(response=="trait change" ~ abs(prediction - dT),
-                            response=="growth" ~ abs(prediction - pcgr)))
+third_plots("cyano")
+third_plots("cilia")
 
-#Now plot, basic plot first: growth
-ggplot(data_preds %>% filter(response=="growth")) + 
-  aes(x=pcgr, y=prediction, col=treat, pch=strain) + #
-  geom_point() +
-  geom_smooth(method="lm", se=F, lwd=0.5) + 
-  #scale_shape_manual(values=0:10) + 
-  theme_bw() + 
-  scale_colour_manual(values=cbPalette) + 
-  facet_grid(vars(form)) + #
-  geom_abline(intercept=0, slope=1)
+## Results ----
 
-#Now plot trait change
-ggplot(data_preds %>% filter(response=="trait change")) + 
-  aes(x=dT, y=prediction, col=treat, pch=strain) + #
-  geom_point() +
-  geom_smooth(method="lm", se=F, lwd=0.5) + 
-  #scale_shape_manual(values=0:10) + 
-  theme_bw() + 
-  scale_colour_manual(values=cbPalette) + 
-  facet_grid(vars(form)) + #
-  geom_abline(intercept=0, slope=1)
+# Ciliates: 
+# Combining two predictors rarely helped to predict growth or trait change to other conditions
 
-#Now error plot
-data_preds_synth <- data_preds %>%
-  group_by(strain, response, predictor, treat) %>%
-  summarise(error = sum(error)) %>%
-  pivot_wider(names_from = predictor, values_from = error) %>%
-  mutate(delta_error = (both - one)/one)
-
-ggplot(data_preds_synth) + 
-  scale_shape_manual(values=0:10) + 
-  theme_bw() + 
-  scale_colour_manual(values=cbPalette) + 
-  aes(x=response, y=delta_error, col=as.factor(treat)) + 
-  #aes(x=response, y=delta_AIC, col=as.factor(treat), pch=trait) + 
-  geom_jitter(width = 0.25) +
-  geom_hline(yintercept = 0) +
-  facet_wrap(vars(strain), ncol=2) + 
-  labs(y=expression(paste("Error"[full],"-Error"[single])), 
-       col="treatment")
-
-###Result--------
-#Ciliates: 
-#Combining two predictors rarely helped to predict growth or trait change to other conditions
-#Cyanos:
-#Combining two predictors rarely helped to predict growth but could improve or worsen trait change prediction.
-
-## Leftovers -------
-## Check correlations between traits and abundance -----
-ggplot(data) + 
-  scale_shape_manual(values=0:10) + 
-  theme_bw() + 
-  scale_colour_manual(values=cbPalette) + 
-  aes(x=log10(density), y=trait, col=as.factor(treat)) + 
-  #aes(x=response, y=delta_AIC, col=as.factor(treat), pch=trait) + 
-  #geom_jitter(width = 0.25) +
-  geom_point(pch=1) +
-  #geom_smooth(method="lm", se=F) +
-  facet_wrap(vars(strain), ncol=2) +
-  labs(col="treatment")
-
-ggsave(paste0("plots/", model_system,"corr.pdf"), 
-       width=5, height = 4, device = "pdf")
+# Cyanos:
+# Combining two predictors rarely helped to predict growth but could improve or worsen trait change prediction.
 
 # Conclusion: not a very strong link between traits and abundance, with some exceptions for the cyanos
 
 ## Basic stats: treatment effects on dd of pcgr and td of dT -----
-stats_result <- modelling(data=data, 
-                          var_to_nest_by = c("strain", "treat"),
-                          formulas=c("dT ~ trait")) %>%#"dT ~ trait", "pcgr ~ density"
-  mutate(model_summary = 
-           list(as_tibble(rownames_to_column(as.data.frame(summary(model)$coefficients),
-                                             var = "predictor")))) %>%
-  unnest(model_summary)  %>%
-  rowwise() %>%
-  mutate(type_of_pred = ifelse(length(grep("density", predictor)>0)|length(grep("trait", predictor)>0), "slope", "intercept")) %>%
-  ungroup() %>%
-  mutate(Estimate_sig = ifelse(`Pr(>|t|)`<0.05, Estimate, NA))
 
-plot <- ggplot(stats_result) +#%>% filter(`Pr(>|t|)`<0.05)
-  theme_bw() + 
-  scale_colour_manual(values=cbPalette) + 
-  aes(x=strain, y=Estimate, col=treat) +
-  geom_point(shape=1, size=3, position=position_dodge(width=0.5)) +
-  geom_errorbar(aes(ymin=Estimate-`Std. Error`, ymax=Estimate+`Std. Error`), 
-                position=position_dodge(width=0.5), width=.2) +
-  geom_point(aes(x=strain, y=Estimate_sig), 
-             position=position_dodge(width=0.5), 
-             pch="*", cex=5, show.legend = F) +
-  geom_hline(yintercept=0, linetype="dashed") +
-  coord_flip() + 
-  facet_wrap(vars(type_of_pred), scales="free") + 
-  theme(axis.text.x = element_text(angle = 90))
+td_general_plots("cyano")
+td_general_plots("cilia")
 
-ggsave(paste("plots/td_general_",model_system,".pdf", sep=""), plot=plot, 
-       width = 7, height = 7)
-#ggsave(paste("plots/td_general_",model_system,".pdf", sep=""), plot=plot, 
-#       width = 5, height = 3)
---
+++ Plotting functions.R
@@ -0,0 +1,273 @@
+# for AIC comparison
+library('qpcR')
+
+basic_plots <- function(model_system) {
+  
+  # GET THE DATA
+  data <- get(paste("data_", model_system, sep = ""))
+  
+  # plot pcgr vs. pop. ----
+  ggplot(data) +
+    theme_bw() + 
+    scale_colour_manual(values = cbPalette) + 
+    aes(x = density, y = pcgr, col = treat) +
+    geom_point() +
+    geom_smooth(method = "lm", se = F, lwd = 0.5) +
+    facet_wrap(vars(strain), ncol = 2, scales = "free") 
+  
+  if (model_system ==  'cyano') {
+    ggsave(paste0("plots/", model_system, "_pcgr.pdf"), 
+           width = 5, height = 4, device = "pdf")
+  } else {
+    ggsave(paste0("plots/", model_system, "_pcgr.pdf"), 
+           width = 5, height = 8, device = "pdf")
+  }
+  
+  # plot dT vs. trait ----
+  ggplot(data) +
+    theme_bw() + 
+    scale_colour_manual(values = cbPalette) + 
+    aes(x = trait, y = dT, col = treat) +
+    geom_point() +
+    geom_smooth(method = "lm", se = F, lwd = 0.5) +
+    facet_wrap(vars(strain), ncol = 2, scales = "free") 
+  
+  if (model_system ==  'cyano') {
+    ggsave(paste0("plots/", model_system, "_dT.pdf"), 
+           width = 5, height = 4, device = "pdf")
+  } else {
+    ggsave(paste0("plots/", model_system, "_dT.pdf"), 
+           width = 5, height = 8, device = "pdf")
+  }
+}
+
+AIC_plots <- function(model_system) {
+  
+  # GET THE DATA
+  data <- get(paste("data_", model_system, sep = ""))
+  
+  stats_result <- modelling(data = data, 
+                            var_to_nest_by = c("strain", "treat"), 
+                            formulas = eqs) %>%
+    dplyr::select(-data)
+  
+  # Now join these models to all the data and make the predictions
+  data_preds <- data %>%
+    ungroup() %>%
+    nest_by(species, strain, treat) %>%
+    left_join(stats_result, by = c("strain", "treat"), multiple = "all") %>%
+    mutate(predictions = list(cbind(data, pred = predict.lm(model, newdata = data)))) %>%
+    rowwise() %>%
+    mutate(AIC = AIC(model)) %>%  
+    ungroup() %>%
+    mutate(response = ifelse(grepl('dT ~ ', form), "trait change", "growth")) %>%
+    ungroup()
+  
+  data_preds_synth <- data_preds %>%
+    mutate(predictors = ifelse(grepl("\\+", form), "both", "focal")) %>%
+    dplyr::select(c("species", "strain", "treat", "response", "AIC", "predictors")) %>%
+    pivot_wider(names_from = "predictors", values_from = "AIC") %>%
+    rowwise %>% 
+    mutate(p.better = akaike.weights(c(both, focal))$weights[1]) 
+  
+  ggplot(data_preds_synth) + 
+    scale_shape_manual(values = 0:10) + 
+    theme_bw() + 
+    scale_colour_manual(values = cbPalette) + 
+    aes(x   = response, y = p.better, fill = treat) + 
+    geom_col(position = position_dodge(width = 0.5), width = 0.25) +
+    geom_hline(yintercept = c(0, 1)) +
+    facet_wrap(vars(strain), ncol = 2, scales = "free") + 
+    labs(col = "treatment", 
+         y = expression("Prob. full model outperforms single model"))
+  
+  if (model_system ==  'cyano') {
+    ggsave(paste0("plots/", model_system, "_AIC.pdf"), 
+           width = 4.5, height = 3.5, device = "pdf")
+  } else {
+    ggsave(paste0("plots/", model_system, "_AIC.pdf"), # ciliate plot bigger
+           width = 4.5, height = 7, device = "pdf")
+  }
+  
+  # Plot model fits
+  data_preds <- data_preds %>%
+    dplyr::select(-c("data", "model")) %>%
+    unnest(predictions)
+  
+  # form == "pcgr ~ density"
+  ggplot(data_preds %>% filter(response == "growth")) + 
+    theme_bw() + 
+    scale_shape_manual(values = 0:10) + 
+    scale_colour_manual(values = cbPalette) + 
+    aes(x = pcgr, y = pred, col = treat, pch = strain) + 
+    geom_point() + 
+    geom_smooth(method = "lm", se = F, lwd = 0.5)+
+    facet_grid(form~.) + #, scales = "free"
+    geom_abline(slope = 1, intercept = 0) + 
+    labs(col = "treatment")
+  
+  if (model_system ==  'cyano') {
+    ggsave(paste0("plots/", model_system, "_growth.pdf"), 
+           width = 4, height = 4.5, device = "pdf")
+  } else {
+    ggsave(paste0("plots/", model_system, "_growth.pdf"), 
+           width = 4, height = 4.5, device = "pdf")
+  }
+  
+  ggplot(data_preds %>% filter(response == "trait change")) + 
+    theme_bw() + 
+    scale_shape_manual(values = 0:10) + 
+    scale_colour_manual(values = cbPalette) + 
+    aes(x = dT, y = pred, col = treat, pch = strain) + 
+    geom_point() + 
+    geom_smooth(method = "lm", se = F, lwd = 0.5)+
+    facet_grid(form~.) + #
+    geom_abline(slope = 1, intercept = 0)  + 
+    labs(col = "treatment")
+  
+  if (model_system ==  'cyano') {
+    ggsave(paste0("plots/", model_system, "_trait.pdf"), 
+           width = 4, height = 4.5, device = "pdf")
+  } else {
+    ggsave(paste0("plots/", model_system, "_trait.pdf"), 
+           width = 4, height = 4.5, device = "pdf")
+  }
+  
+}
+
+third_plots <- function(model_system) {
+  
+  # GET THE DATA
+  data <- get(paste("data_", model_system, sep = ""))
+  
+  stats_result <- modelling(data = data %>% filter(treat == "C"), 
+                            var_to_nest_by = c("strain"),
+                            formulas = c("dT ~ trait", 
+                                       "dT ~ trait + density",
+                                       "pcgr ~ density",
+                                       "pcgr ~ trait + density")) %>%
+    dplyr::select(-data)
+  
+  #Now join these models to all the data and make the predictions
+  data_preds <- data %>%
+    ungroup() %>%
+    nest_by(strain) %>%
+    left_join(stats_result, by = c("strain"), multiple = "all") %>%
+    mutate(response = case_when(length(grep("dT", form)) > 0 ~ "trait change",
+                                length(grep("pcgr", form)) > 0 ~ "growth")) %>%
+    mutate(predictor = case_when(length(grep("trait + density", form, fixed = T)) > 0 ~ "both",
+                                 length(grep("density", form)) > 0 ~ "one",
+                                 length(grep("trait", form)) > 0 ~ "one")) %>%
+    #dplyr::select(-form) %>%
+    mutate(data_and_pred = list(cbind(
+      data, 
+      prediction = predict.lm(object = model, newdata = data)))) %>%
+    dplyr::select(c("strain", "response", "form", "predictor", "data_and_pred")) %>%
+    unnest(c("data_and_pred")) %>%
+    mutate(error = case_when(response == "trait change" ~ abs(prediction - dT),
+                              response == "growth" ~ abs(prediction - pcgr)))
+  
+  #Now plot, basic plot first: growth
+  ggplot(data_preds %>% filter(response == "growth")) + 
+    aes(x = pcgr, y = prediction, col = treat, pch = strain) + #
+    geom_point() +
+    geom_smooth(method = "lm", se = F, lwd = 0.5) + 
+    theme_bw() + 
+    scale_colour_manual(values = cbPalette) + 
+    facet_grid(form~.) + #
+    geom_abline(intercept = 0, slope = 1)
+  
+  #Now plot trait change
+  ggplot(data_preds %>% filter(response == "trait change")) + 
+    aes(x = dT, y = prediction, col = treat, pch = strain) + #
+    geom_point() +
+    geom_smooth(method = "lm", se = F, lwd = 0.5) + 
+    theme_bw() + 
+    scale_colour_manual(values = cbPalette) + 
+    facet_grid(form~.) + #
+    geom_abline(intercept = 0, slope = 1)
+  
+  #Now error plot
+  data_preds_synth <- data_preds %>%
+    group_by(strain, response, predictor, treat) %>%
+    summarise(error = sum(error)) %>%
+    pivot_wider(names_from = predictor, values_from = error) %>%
+    mutate(delta_error = (both - one)/one)
+  
+  ggplot(data_preds_synth) + 
+    scale_shape_manual(values = 0:10) + 
+    theme_bw() + 
+    scale_colour_manual(values = cbPalette) + 
+    aes(x = response, y = delta_error, col = treat) + 
+    geom_jitter(width = 0.25) +
+    geom_hline(yintercept = 0) +
+    facet_wrap(vars(strain), ncol = 2) + 
+    labs(y = expression(paste("Error"[full],"-Error"[single])), 
+         col = "treatment")
+ 
+  # Leftovers ----
+  
+  ## Check correlations between traits and abundance ----
+  
+  ggplot(data) + 
+    scale_shape_manual(values = 0:10) + 
+    theme_bw() + 
+    scale_colour_manual(values = cbPalette) + 
+    aes(x = log10(density), y = trait, col = treat) + 
+    geom_point(pch = 1) +
+    facet_wrap(vars(strain), ncol = 2) +
+    labs(col = "treatment")
+  
+  if (model_system == 'cyano') {
+    ggsave(paste0("plots/", model_system,"_corr.pdf"), 
+           width = 5, height = 4, device = "pdf")
+  } else {
+    ggsave(paste0("plots/", model_system,"_corr.pdf"), 
+           width = 5, height = 8, device = "pdf")
+  }
+}
+
+td_general_plots <- function(model_system) {
+  
+  # GET THE DATA
+  data <- get(paste("data_", model_system, sep = ""))
+  
+  stats_result <- modelling(data = data, 
+                            var_to_nest_by = c("strain", "treat"),
+                            formulas = c("dT ~ trait")) %>%
+    mutate(model_summary = 
+             list(as_tibble(rownames_to_column(as.data.frame(summary(model)$coefficients),
+                                               var = "predictor")))) %>%
+    unnest(model_summary)  %>%
+    rowwise() %>%
+    mutate(type_of_pred = ifelse(
+      length(grep("density", predictor) > 0) | length(grep("trait", predictor) > 0), 
+      "slope", "intercept")) %>%
+    ungroup() %>%
+    mutate(Estimate_sig = ifelse(`Pr(>|t|)` < 0.05, Estimate, NA))
+  
+  plot <- ggplot(stats_result) +
+    theme_bw() + 
+    scale_colour_manual(values = cbPalette) + 
+    aes(x = strain, y = Estimate, col = treat) +
+    geom_point(shape = 1, size = 3, position = position_dodge(width = 0.5)) +
+    geom_errorbar(aes(ymin = Estimate - `Std. Error`, ymax = Estimate + `Std. Error`), 
+                  position = position_dodge(width = 0.5), width = .2) +
+    geom_point(aes(x = strain, y = Estimate_sig), 
+               position = position_dodge(width = 0.5), 
+               pch = "*", cex = 5, show.legend = F) +
+    geom_hline(yintercept = 0, linetype = "dashed") +
+    coord_flip() + 
+    facet_wrap(vars(type_of_pred), scales = "free") + 
+    theme(axis.text.x = element_text(angle = 90)) + 
+    labs(col = "treatment")
+  
+  if (model_system == 'cyano') {
+    ggsave(paste("plots/", model_system,"_td_general.pdf", sep = ""), plot = plot, 
+           width = 7, height = 7)
+  } else {
+    ggsave(paste("plots/", model_system,"_td_general.pdf", sep = ""), plot = plot, 
+           width = 7, height = 7)
+  }
+  
+}
